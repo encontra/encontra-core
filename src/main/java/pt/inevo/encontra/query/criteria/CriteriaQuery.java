493c5709e9b0300e2ff84bd57464708879092cad
nelson.silva <nelson.silva@inevo.pt>
2010-09-15 17:51:22 +0100

    Starting work on CriteriaQuery

--- src/main/java/pt/inevo/encontra/query/criteria/CriteriaQuery.java
+++ src/main/java/pt/inevo/encontra/query/criteria/CriteriaQuery.java
@@ -1,14 +1,20 @@
 package pt.inevo.encontra.query.criteria;
 
-import pt.inevo.encontra.query.Query;
 
 /**
  * Criteria Query - Query for combining Criteria
  * @author ricardo
  */
-public class CriteriaQuery extends Query {
+public interface CriteriaQuery<T>{
+    public CriteriaQuery<T> where(Expression<Boolean> restriction);
 
-    public CriteriaQuery(){
-        super.type = QueryType.CRITERIA;
-    }
+    public CriteriaQuery<T> where(Predicate... restrictions);
+
+    public CriteriaQuery<T> distinct(boolean distinct);
+
+    public CriteriaQuery<T> orderBy(Order... orders);
+
+    public Class<T> getResultType();
+
+    public <X> Path<X> from(Class<X> cls);
 }
